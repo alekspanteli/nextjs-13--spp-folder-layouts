67f9746714a2b7d156706471357ebdde67fa9c2b
alekspanteli <alekspanteli@rambler.ru>
2022-11-12 08:51:55 +0000

    sd

--- app/navbar.js
+++ app/navbar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
         <header className='container flex justify-between items-center py-5'>
             <Link href="/">Home</Link>
             <nav className='flex gap-3'>
-                <Link href="/">Home</Link>
+                <Link href="/">sd</Link>
                 <Link href="/posts">Blog</Link>
             </nav>
         </header>
